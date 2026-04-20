0311f78e862fc8f57d78cb27c82932c9b714840f
GabrielRodriguezGil <gabrielrodgil088@gmail.com>
2026-04-20 14:17:18 +0200

    feat(ScoreCard): :sparkles: Primeros pasos del scorecard, constructor y sus atributos.

---
+++ app/src/main/java/edu/teamrocket/brunosbox/ScoreCard.java
@@ -0,0 +1,31 @@
+package edu.teamrocket.brunosbox;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ScoreCard {
+
+    private final String color;
+    private String redCorner = "";
+    private String blueCorner = "";
+    private String[] judgeScoreCard;
+    private Byte redCornerFinalScore;
+    private Byte blueCornerFinalScore;
+
+    private List<Round> rounds = new ArrayList<Round>();
+
+    public ScoreCard(String color) {
+        this.color = color;
+    }
+
+    public void setRedCorner(String boxerName) {
+        this.redCorner = boxerName;
+    }
+
+    public void setBlueCorner(String boxerName) {
+        this.blueCorner = boxerName;
+    }
+
+}
