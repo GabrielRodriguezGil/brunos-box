a841ac252dfd3326db23589fb640612ece27aabc
MiguelPhino <mpahinogutierrez@gmail.com>
2026-04-22 13:55:20 +0200

    refactor(round): :recycle: Cambiar el  módulo a una interfaz

--- app/src/main/java/edu/teamrocket/brunosbox/Round.java
+++ app/src/main/java/edu/teamrocket/brunosbox/Round.java
@@ -1,10 +1,7 @@
 package edu.teamrocket.brunosbox;
 
-public class Round {
-    byte  getRedBoxerScore = 0;
-    byte  getBlueBoxerScore = 0;
-
-    byte[] boxerRoundScore(){
-        return new byte[]{this.getBlueBoxerScore, this.getRedBoxerScore};
-    } 
+interface Round {
+    
+    byte  getRedBoxerScore();
+    byte  getBlueBoxerScore();
 }
