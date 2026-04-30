3ae64ffa5d2dfb3c0c1dbb68a3cd09886c0f76ce
MiguelPahino <mpahinogutierrez@gmail.com>
2026-04-30 19:09:02 +0200

    feat(Boxer): :sparkles: Añadir el ENUM Boxer para poder refactorizar los Rounds

---
+++ app/src/main/java/edu/teamrocket/brunosbox/Boxer.java
@@ -0,0 +1,17 @@
+package edu.teamrocket.brunosbox;
+
+enum Boxer {
+    RED((byte)0),
+    BLUE((byte)0);
+
+    private final byte corner;
+
+    private Boxer(byte corner){
+        this.corner = corner;
+    }
+
+    public byte corner(){
+        return this.corner;
+    }
+
+}
